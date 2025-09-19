ee2329b6e207e5b94267a2bb44b254ed508cbb46
Ardentto <dkuful@gmail.com>
2025-09-19 09:26:56 +0300

    Add files via upload

---
+++ task2.cpp
@@ -0,0 +1,46 @@
+//Лабораторная по ОАиП
+#include <iostream>
+using namespace std;
+
+int main() {
+    const int size = 10;
+    int arr[size];
+    cout << "Введите 10 целых чисел:\n";
+    for (int i = 0; i < size; i++) {
+        cin >> arr[i];
+    }
+
+    int sum = 0;
+    for (int i = 0; i < size; i++) {
+        sum += arr[i];
+    }
+    cout << "Сумма элементов: " << sum << endl;
+    
+
+    int max = -123124;
+    for (int i = 1; i < size; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    cout << "Максимальный элемент: " << max << endl;
+    
+    for (int i = 1; i < size; i++) {
+        int pred = arr[i];
+        int j = i - 1;
+        
+        while (j >= 0 && arr[j] < pred) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = pred;
+    }
+
+    cout << "Отсортированный массив: ";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    
+    return 0;
+}
