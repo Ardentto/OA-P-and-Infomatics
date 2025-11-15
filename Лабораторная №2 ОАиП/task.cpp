26df23241246b77d91b947eb306b3a16896bb98e
Ardentto <dkuful@gmail.com>
2025-11-15 12:54:35 +0300

    task.cpp

--- Лабораторная №2 ОАиП/task.cpp
+++ Лабораторная №2 ОАиП/task.cpp
@@ -1,97 +1,127 @@
 #include <iostream>
 #include <string>
-#include <vector>
-#include <sstream>
-#include <algorithm>
-#include <cctype>
 
-std::string longestCommonSubstring(const std::string& str1, const std::string& str2) {
-    int m = str1.length();
-    int n = str2.length();
-    std::vector<std::vector<int>> dp(m + 1, std::vector<int>(n + 1, 0));
-    int maxLength = 0;
-    int endPos = 0;
+// Простая функция для поиска общей подстроки
+std::string findCommonSubstring(const std::string& s1, const std::string& s2) {
+    std::string longest = "";
+    
+    // Простой перебор всех возможных подстрок
+    for (int i = 0; i < s1.length(); i++) {
+        for (int j = 0; j < s2.length(); j++) {
+            int k = 0;
+            // Сравниваем символы пока они совпадают
+            while (i + k < s1.length() && j + k < s2.length() && 
+                   s1[i + k] == s2[j + k]) {
+                k++;
+            }
+            // Если нашли более длинную подстроку
+            if (k > longest.length()) {
+                longest = s1.substr(i, k);
+            }
+        }
+    }
+    
+    return longest;
+}
 
-    for (int i = 1; i <= m; i++) {
-        for (int j = 1; j <= n; j++) {
-            if (str1[i - 1] == str2[j - 1]) {
-                dp[i][j] = dp[i - 1][j - 1] + 1;
-                if (dp[i][j] > maxLength) {
-                    maxLength = dp[i][j];
-                    endPos = i - 1;
+int main() {
+    std::string input;
+    std::cout << "Введите 10 слов: ";
+    std::getline(std::cin, input);
+
+    // Простой разбор строки на слова
+    std::string words[10];
+    int wordCount = 0;
+    std::string currentWord = "";
+    
+    for (char c : input) {
+        if (c == ' ') {
+            if (!currentWord.empty()) {
+                words[wordCount] = currentWord;
+                wordCount++;
+                currentWord = "";
+                if (wordCount >= 10) break;
+            }
+        } else {
+            currentWord += c;
+        }
+    }
+    // Добавляем последнее слово
+    if (!currentWord.empty() && wordCount < 10) {
+        words[wordCount] = currentWord;
+        wordCount++;
+    }
+
+    if (wordCount < 10) {
+        std::cout << "Нужно 10 слов!" << std::endl;
+        return 1;
+    }
+
+    // Подсчет статистики
+    int wordsWithB = 0;
+    int maxWordLength = 0;
+    int dInLastWord = 0;
+    int matchingChars = 0;
+    
+    // Преобразование в верхний регистр
+    std::string upperString = "";
+    for (char c : input) {
+        if (c >= 'a' && c <= 'z') {
+            upperString += c - 32;  // в верхний регистр
+        } else {
+            upperString += c;
+        }
+    }
+
+    // Обработка слов
+    for (int i = 0; i < 10; i++) {
+        const std::string& w = words[i];
+        
+        // Слова на 'b'
+        if (!w.empty() && (w.back() == 'b' || w.back() == 'B')) {
+            wordsWithB++;
+        }
+        
+        // Самое длинное слово
+        if (w.length() > maxWordLength) {
+            maxWordLength = w.length();
+        }
+        
+        // Совпадает второй и предпоследний символ
+        if (w.length() >= 4 && w[1] == w[w.length() - 2]) {
+            matchingChars++;
+        }
+        
+        // Буквы 'd' в последнем слове
+        if (i == 9) {
+            for (char c : w) {
+                if (c == 'd' || c == 'D') {
+                    dInLastWord++;
                 }
             }
         }
     }
 
-    return maxLength == 0 ? "" : str1.substr(endPos - maxLength + 1, maxLength);
-}
-
-int main() {
-    std::string input;
-    std::cout << "Введите строку из 10 слов на латинице: ";
-    std::getline(std::cin, input);
-
-    std::vector<std::string> words;
-    std::stringstream ss(input);
-    std::string word;
-    
-    while (ss >> word) {
-        words.push_back(word);
-    }
-
-    if (words.size() < 10) {
-        std::cout << "Ошибка: нужно ввести минимум 10 слов!" << std::endl;
-        return 1;
-    }
-
-    int countEndWithB = 0;
-    int countDInLast = 0;
-    size_t maxLength = 0;
-    int countMatchingChars = 0;
-
-    for (size_t i = 0; i < words.size(); i++) {
-        const auto& w = words[i];
-        
-        if (!w.empty() && std::tolower(w.back()) == 'b') {
-            countEndWithB++;
-        }
-        
-        if (w.length() > maxLength) {
-            maxLength = w.length();
-        }
-        
-        if (w.length() >= 4 && w[1] == w[w.length() - 2]) {
-            countMatchingChars++;
-        }
-        
-        if (i == words.size() - 1) {
-            for (char c : w) {
-                if (std::tolower(c) == 'd') countDInLast++;
-            }
-        }
-    }
-
-    std::string upperCaseString = input;
-    std::transform(upperCaseString.begin(), upperCaseString.end(), upperCaseString.begin(), ::toupper);
-
+    // Поиск самой длинной общей подстроки
     std::string longestCommon = "";
-    for (size_t i = 0; i < words.size(); i++) {
-        for (size_t j = i + 1; j < words.size(); j++) {
-            std::string common = longestCommonSubstring(words[i], words[j]);
+    for (int i = 0; i < 10; i++) {
+        for (int j = i + 1; j < 10; j++) {
+            std::string common = findCommonSubstring(words[i], words[j]);
             if (common.length() > longestCommon.length()) {
                 longestCommon = common;
             }
         }
     }
 
+    // Вывод результатов
     std::cout << "\nРезультаты:\n";
-    std::cout << "1. Слова на 'b': " << countEndWithB << std::endl;
-    std::cout << "2. Длина самого длинного слова: " << maxLength << std::endl;
-    std::cout << "3. Букв 'd' в последнем слове: " << countDInLast << std::endl;
-    std::cout << "4. В верхнем регистре: " << upperCaseString << std::endl;
-    std::cout << "5. Совпадает второй и предпоследний: " << countMatchingChars << std::endl;
-    std::cout << "6. Самая длинная общая подстрока: " << (longestCommon.empty() ? "нет" : longestCommon) << std::endl;
+    std::cout << "1. Слова на 'b': " << wordsWithB << std::endl;
+    std::cout << "2. Длина самого длинного слова: " << maxWordLength << std::endl;
+    std::cout << "3. Букв 'd' в последнем слове: " << dInLastWord << std::endl;
+    std::cout << "4. В верхнем регистре: " << upperString << std::endl;
+    std::cout << "5. Совпадает второй и предпоследний: " << matchingChars << std::endl;
+    std::cout << "6. Самая длинная общая подстрока: " << 
+        (longestCommon.empty() ? "нет" : longestCommon) << std::endl;
 
     return 0;
 }
